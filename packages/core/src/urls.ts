69cbfeddc43166e4a5a8acb35d1201c7fb4a3a0e
Charles Gruenais <charles.gruenais@gmail.com>
2023-11-01 18:00:50 +0100

    fix: changed module id parsing method

--- packages/core/src/urls.ts
+++ packages/core/src/urls.ts
@@ -7,6 +7,7 @@ export const addSourceQuerySuffix = (id: string) => {
 
   const params = new URLSearchParams(idParams);
   params.set(SOURCE_QUERY_SUFFIX, '1');
+
   return `${sourceId}?${params.toString()}`;
 };
 
@@ -18,6 +19,7 @@ export const parseId = (id: string) => {
   const params = new URLSearchParams(idParams);
   const serveSource = params.get(SOURCE_QUERY_SUFFIX) === '1';
   params.delete(SOURCE_QUERY_SUFFIX);
+
   const url = params.size ? `${sourceId}?${params.toString()}` : sourceId;
   return { url, serveSource };
 };
