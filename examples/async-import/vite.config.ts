1e2d648f0355a2c8bf51105226afdceaedfbae4c
Charles Gruenais <charles.gruenais@gmail.com>
2024-04-28 01:50:20 +0200

    chore: add async import example (issue #37)

---
+++ examples/async-import/vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+import vue from '@vitejs/plugin-vue';
+import EntryShakingPlugin from 'vite-plugin-entry-shaking';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const pathToLib = resolve(__dirname, './src/lib');
+
+export default defineConfig(async () => ({
+  plugins: [
+    await EntryShakingPlugin({
+      targets: [pathToLib],
+      debug: true,
+    }),
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@lib': pathToLib,
+    },
+  },
+}));
